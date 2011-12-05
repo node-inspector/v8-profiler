cdcd649f8b9a7c4943058c766d138b0b77648788
Danny Coates <dannycoates@gmail.com>
2011-12-05 14:22:48 -0800

    added some snapshot functions

--- v8-profiler.js
+++ v8-profiler.js
@@ -1,14 +1,40 @@
-var binding = require("./build/default/profiler");
+var binding = require("./build/Release/profiler");
 
 function Snapshot() {}
 
-Snapshot.prototype.children = function(node, order) {
-  var i, children = [], order = order || "retainedSize";
+Snapshot.prototype.compare = function (other) {
+  var my_objects = this.nodeCounts(),
+      their_objects = other.nodeCounts(),
+      diff = {}, i, k, my_val, their_val;
+      all_keys = Object.keys(my_objects).concat(Object.keys(their_objects)); //has dupes, oh well
+  for (i = 0; i < all_keys.length; i++) {
+    k = all_keys[i];
+    my_val = my_objects[k] || 0;
+    their_val = their_objects[k] || 0;
+    diff[k] = their_val - my_val;
+  }
+  return diff;
+}
+
+Snapshot.prototype.hotPath = function() {
+  var path = [], node = this.root, c, i = 0;
+  c = this.children(node);
+  while (c.length > 0 && i < 1000) {
+    node = c[0].to;
+    c = this.children(node);
+    path.push(node);
+    i++;
+  }
+  return path;
+}
+
+Snapshot.prototype.children = function(node) {
+  var i, children = [];
   for(i = 0; i < node.childrenCount; i++) {
     children[i] = node.getChild(i);
   }
   children.sort(function (a, b){
-    return a[order] - b[order];
+    return b.to.retainedSize() - a.to.retainedSize();
   });
   return children;
 }
@@ -27,24 +53,48 @@ Snapshot.prototype.topDominatorIds = function() {
   arr = Object.keys(doms).map(function(d){
     return {id: d, count: doms[d]};
   });
-  arr.sort(funtion(a, b) {
-    return a.count - b.count;
+  arr.sort(function(a, b) {
+    return b.count - a.count;
   });
   return arr;
 }
 
 Snapshot.prototype.topDominators = function() {
-  return this.topDominatorIds().map(function(id){
-    return this.getNodeById(id);
+  var self = this;
+  return this.topDominatorIds().map(function(d){
+    return self.getNodeById(+d.id);
   });
 }
 
 Snapshot.prototype.allNodes = function() {
-  var n = this.nodesCount, i, nodes;
+  var n = this.nodesCount, i, nodes = [];
   for (i = 0; i < n; i++) {
     nodes[i] = this.getNode(i);
   }
   return nodes;
+}
+
+Snapshot.prototype.nodeCounts = function() {
+  var objects = {};
+  this.allNodes().forEach(function(n){
+    if(n.type === "Object") {
+      if (objects[n.name]) {
+        objects[n.name] += 1;
+      }
+      else {
+        objects[n.name] = 1;
+      }
+    }
+    else {
+      if (objects[n.type]) {
+        objects[n.type] += 1;
+      }
+      else {
+        objects[n.type] = 1;
+      }
+    }
+  });
+  return objects;
 }
 
 //adapted from WebCore/bindings/v8/ScriptHeapSnapshot.cpp
